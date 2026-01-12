7e02b667f22be877780b6b9b48d5e2c6e9aef299
Aakash Kattelu <aakash.kattelu@gmail.com>
2026-01-12 13:54:57 -0500

    refactor: refactor to lib/corpus.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 local Filter = require("lib.filter")
 local Dial = require("ui.dial")
+local Corpus = require("lib.corpus")
 
 -- State
 local words = {}
@@ -91,27 +92,7 @@ function love.load()
   love.graphics.setFont(font)
 
   -- Load word list
-  local content = love.filesystem.read("words.txt")
-  if content then
-    for word in content:gmatch("[^\r\n]+") do
-      if #word > 0 then
-        table.insert(words, word:lower())
-      end
-    end
-  else
-    -- Fallback test words
-    words = { "the", "be", "to", "of", "and", "a", "in", "that", "have", "it",
-      "for", "not", "on", "with", "he", "as", "you", "do", "at", "this",
-      "but", "his", "by", "from", "they", "we", "say", "her", "she", "or",
-      "an", "will", "my", "one", "all", "would", "there", "their", "what",
-      "so", "up", "out", "if", "about", "who", "get", "which", "go", "me",
-      "function", "return", "const", "let", "var", "string", "number",
-      "select", "filter", "search", "find", "start", "stop", "send",
-      "think", "thank", "throw", "through", "then", "than", "ten", "ton",
-      "token", "taken", "turn", "twin", "tension", "transition", "train" }
-  end
-
-  print("Loaded " .. #words .. " words")
+  words = Corpus.load()
   filtered = words
 
   -- Create and initialize dial instances
